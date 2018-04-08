eea97d8394acefd1e8accd9c71700d1ab053fadc
buildspeakers <sudobuildspeakers@gmail.com>
2018-04-08 22:57:19 +0100

    added no results message

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,12 @@
-let tl = new TimelineMax();
+let tl = new TimelineMax
+let resultsDiv = document.querySelector('#results');
+
+function noResults(){
+  let noResultsMessage = `
+  <h1>Nothing found</h1>
+  <h3>try again</h3>`;
+  resultsDiv.innerHTML = noResultsMessage;
+}
 
 // send req
 $('#search').submit(function(event){
@@ -17,41 +25,43 @@ $('#search').submit(function(event){
     dataFormat: 'json',
     success: function(data){
       let results = data.query.search;
-      let resultFigures = [];
-      for (let i = 0; i < results.length; i++){
-        let fullSnippet;
-        if (results[i].snippet[0] !== results[i].snippet[0].toUpperCase() ) {
-          fullSnippet = '....' + results[i].snippet;
-        }
-        else fullSnippet = results[i].snippet;
-        resultFigures.push(`
-          <figure class="result">
-            <div class="frame">
-              <img src="moose.jpg">
-              <h3>${results[i].title}</h3>
-            </div>
-            <figcaption>
-              <p>${fullSnippet}</p>
-              <a target="_blank" href="https://en.wikipedia.org/wiki/${results[i].title}">Full Article</a>
-            </figcaption>
-          </figure>`);
-      };
-      let target = document.querySelector('#results');
-      // build string
-      let allFigures = '';
-      for (let i = 1; i < resultFigures.length; i++) {
-        allFigures += resultFigures[i];
-      };
-      // write to DOM
-      target.innerHTML = allFigures;
+      if (data.query.searchinfo.totalhits == 0) noResults();
+      else {
+        let resultFigures = [];
+        for (let i = 0; i < results.length; i++){
+          let fullSnippet;
+          if (results[i].snippet[0] !== results[i].snippet[0].toUpperCase() ) {
+            fullSnippet = '....' + results[i].snippet;
+          }
+          else fullSnippet = results[i].snippet;
+          resultFigures.push(`
+            <figure class="result">
+              <div class="frame">
+                <img src="moose.jpg">
+                <h3>${results[i].title}</h3>
+              </div>
+              <figcaption>
+                <p>${fullSnippet}</p>
+                <a target="_blank" href="https://en.wikipedia.org/wiki/${results[i].title}">Full Article</a>
+              </figcaption>
+            </figure>`);
+        };
 
-      // fade in one by one
-      let resultsArray = Array.from(document.querySelectorAll('.result'));
-      tl.staggerTo(resultsArray, 0.2, { // 0.05 = length of each animation event
-        opacity: 1,
-        x: 0,
-      }, 0.05);
+        // build string
+        let allFigures = '';
+        for (let i = 1; i < resultFigures.length; i++) {
+          allFigures += resultFigures[i];
+        };
+        // write to DOM
+        resultsDiv.innerHTML = allFigures;
 
+        // fade in one by one
+        let resultsArray = Array.from(document.querySelectorAll('.result'));
+        tl.staggerTo(resultsArray, 0.2, { // 0.05 = length of each animation event
+          opacity: 1,
+          x: 0,
+        }, 0.05);
+      }
     } // end success
   }); // end AJAX
 }); // end form submit
